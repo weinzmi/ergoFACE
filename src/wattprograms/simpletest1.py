fa622e6c5397d64ef44067725a13c50eed505f4d
weinzmi <46070116+weinzmi@users.noreply.github.com>
2018-12-26 09:52:15 +0100

    Update simpletest1.py

--- src/wattprograms/simpletest1.py
+++ src/wattprograms/simpletest1.py
@@ -5,7 +5,5 @@ program = yaml.safe_load(f)
 f.close()
 
 for program_id in program['Programtest']:
-    sequence = program['Programtest'][program_id]['Watt']
-    
-    
-print sequence
+    sequence = program['Programtest'][program_id]['Watt'] 
+    print sequence
