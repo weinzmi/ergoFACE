2d14801af257a015ee981d7ec0b740625d9e8faf
weinzmi <46070116+weinzmi@users.noreply.github.com>
2018-12-26 09:54:33 +0100

    Update simpletest1.py

--- src/wattprograms/simpletest1.py
+++ src/wattprograms/simpletest1.py
@@ -1,9 +1,12 @@
-import yaml
+import yaml, time
 
 f = open("programtest.yaml")
 program = yaml.safe_load(f)
 f.close()
 
 for program_id in program['Programtest']:
-    sequence = program['Programtest'][program_id]['Watt'] 
-    print sequence
+    duration = program['Programtest'][program_id]['Duration'] 
+    watt = program['Programtest'][program_id]['Watt'] 
+    print watt
+    time.sleep(duration)
+    
