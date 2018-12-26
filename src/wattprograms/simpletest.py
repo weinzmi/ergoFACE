bfa11c537d48d64a5a39ec9061e5235597251125
weinzmi <46070116+weinzmi@users.noreply.github.com>
2018-12-26 09:07:02 +0100

    Update simpletest.py

--- src/wattprograms/simpletest.py
+++ src/wattprograms/simpletest.py
@@ -3,21 +3,27 @@
 import yaml
 import os, sys
 
-#with open("programtest.yaml", 'r') as ymlfile:
-#    cfg = yaml.load(ymlfile)
+with open("programtest.yaml", 'r') as ymlfile:
+    cfg = yaml.load(ymlfile)
+
+    try:
+        print cfg["Programtest"]["Seq1"]["Duration"]
+    except:
+        print "Problem"
+
 
 #for section in cfg:
-#    print(section)
+ #   print(section)
 #print(cfg['Seq1'])
 #print(cfg['Seq2'])
 
 
-for filename in os.listdir(currentPath):
-            print filename
-            if(filename.endswith(".yaml")):
-                with open(os.path.join(currentPath, filename)) as myFile:
-                    results = yaml.load(myFile)
-                    try:
-                        print results["Programtest"]["Seq1"]["Duration"]
-                    except:
-                        print "Problem"
+#for filename in os.listdir(currentPath):
+#            print filename
+#            if(filename.endswith(".yaml")):
+#                with open(os.path.join(currentPath, filename)) as myFile:
+#                    results = yaml.load(myFile)
+#                    try:
+#                        print results["Programtest"]["Seq1"]["Duration"]
+#                    except:
+#                        print "Problem"
