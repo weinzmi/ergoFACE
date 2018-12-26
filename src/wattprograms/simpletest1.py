a511603dd9206856ddc7c476a1cafec9a9b15c3e
weinzmi <46070116+weinzmi@users.noreply.github.com>
2018-12-26 09:46:49 +0100

    Create simpletest1.py

---
+++ src/wattprograms/simpletest1.py
@@ -0,0 +1,11 @@
+import yaml
+
+f = open("programtest.yaml")
+program = yaml.safe_load(f)
+f.close()
+
+for program_id in program['Programtest']:
+    sequence = program['Programtest'][program_id]['Watt']
+    
+    
+print sequence
