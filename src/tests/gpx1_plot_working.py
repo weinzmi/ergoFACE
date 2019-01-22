c588f17a42c361210311e8f69bfbc9933e0d0e53
weinzmi <46070116+weinzmi@users.noreply.github.com>
2019-01-22 22:33:07 +0100

    gpx.py SIM mode for gpx files calculating the power for gradient and environmental setings

--- src/tests/gpx1_plot_working.py
+++ src/tests/gpx1_plot_working.py
@@ -3,6 +3,7 @@ import gpxpy.gpx
 import gpxpy.geo as mod_geo
 import time
 import math
+import matplotlib.pyplot as plt
 import pandas as pd
 import os
 import sys
@@ -111,6 +112,16 @@ def gpx_run():
                     # calculate gradient between points
                     gradient = elevation / distance * 100
 
+                # test with simulated velocity of 20km/h == 5.55 m/s
+                v = 5.55
+
+                # inputs for power calculation
+                G = gradient/100
+
+                # output of power calculation
+                P = E * (M * g * v * math.cos(math.atan(G)) * Crr + M * g * v
+                         * math.sin(math.atan(G)) + 1/2*p * CD * A * v**3)
+
                 """
                 the simulation is close to the calculator found here:
                 https://www.gribble.org/cycling/power_v_speed.html
@@ -123,9 +134,14 @@ def gpx_run():
                 Power:   194.82 W
 
                 """
+                # this is the procesing time for each point to point (segment)
+                # but what if the speed (RPM) changes in between this time?
+                # this will not work, but good for simulation with constant speed
+                # another for loop will be neccesarry with a sleep of 1sec for
+                # recalculation of dynamic sleep time based on
+                # the refreshed velocity
 
-                # load gpx information into a datafild for
-                # calculation and plotting
+                # load gpx information into a datafild for calculation and plotting
 
                 df = df.append({'lon': point.longitude,
                                 'lat': point.latitude,
@@ -133,28 +149,74 @@ def gpx_run():
                                 'time': point.time,
                                 'dist': distance,
                                 'ele': elevation,
-                                'grad': gradient},
+                                'grad': gradient,
+                                'pow': P},
                                ignore_index=True)
 
+                # time.sleep(wait)
+
+    # start of plotting
+    def make_patch_spines_invisible(ax):
+        ax.set_frame_on(True)
+        ax.patch.set_visible(False)
+        for sp in ax.spines.values():
+            sp.set_visible(False)
+
+    fig, host = plt.subplots()
+    fig.subplots_adjust(right=0.75)
+
+    par1 = host.twinx()
+    par2 = host.twinx()
+
+    # Offset the right spine of par2.  The ticks and label have already been
+    # placed on the right by twinx above.
+    par2.spines["right"].set_position(("axes", 1.2))
+    # Having been created by twinx, par2 has its frame off, so the line of its
+    # detached spine is invisible.  First, activate the frame but make the patch
+    # and spines invisible.
+    make_patch_spines_invisible(par2)
+    # Second, show the right spine.
+    par2.spines["right"].set_visible(True)
+
+    p1, = host.plot(df['time'], df['alt'], "b-", label="Altitude")
+    p2, = par1.plot(df['time'], df['ele'], "r-", label="Elevation")
+    p3, = par2.plot(df['time'], df['pow'], "g-", label="Power")
+
+    # host.set_xlim(0, 2)
+    # host.set_ylim(0, 2)
+    # par1.set_ylim(0, 4)
+    # par2.set_ylim(1, 65)
+
+    host.set_xlabel("Time")
+    host.set_ylabel("Altitude")
+    par1.set_ylabel("Elevation")
+    par2.set_ylabel("Power")
+
+    host.yaxis.label.set_color(p1.get_color())
+    par1.yaxis.label.set_color(p2.get_color())
+    par2.yaxis.label.set_color(p3.get_color())
+
+    tkw = dict(size=4, width=1.5)
+    host.tick_params(axis='y', colors=p1.get_color(), **tkw)
+    par1.tick_params(axis='y', colors=p2.get_color(), **tkw)
+    par2.tick_params(axis='y', colors=p3.get_color(), **tkw)
+    host.tick_params(axis='x', **tkw)
+
+    lines = [p1, p2, p3]
+
+    host.legend(lines, [l.get_label() for l in lines])
+
+    plt.show()
+
     for index, row in df.iterrows():
-        # define cylce in 10th of wait
-        cycle = 10
-        # recalculation of dynamic sleep time based on
-        # the refreshed velocity
-        for i in range(0, 10):
-            # test with simulated velocity of 20km/h == 5.55 m/s
-            v = 5.55
-            G = row['grad'] / 100
-            # output of power calculation
-            P = E * (M * g * v * math.cos(math.atan(G)) * Crr + M * g * v
-                     * math.sin(math.atan(G)) + 1/2*p * CD * A * v**3)
+        print("Gradient:  ", f"{row['grad']:4.2f}", "%", "Power:   ",
+              f"{row['pow']:4.2f}", "W")
 
-            print("Gradient:  ", f"{row['grad']:4.2f}", "%", "Power:   ",
-                  f"{P:4.2f}", "W")
+        wait = row['dist'] / v
 
-            wait = row['dist'] / v / cycle
-            print("Wait:      ", f"{wait:4.2f}", "s")
-            time.sleep(wait)
+        print("Wait:      ", f"{wait:4.2f}", "s")
+
+        time.sleep(wait)
 
 
 module_load()
